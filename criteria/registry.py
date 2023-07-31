c0771b16c96eb263d745449eafa8b623574459fd
Soyun Choi <82935838+soyunchoi@users.noreply.github.com>
2023-07-31 17:47:42 +0900

    Delete criteria directory

--- criteria/registry.py
+++
@@ -1,8 +0,0 @@
-import torch.nn as nn
-
-from rgbd_seg.utils import Registry
-
-CRITERIA = Registry('criterion')
-
-CrossEntropyLoss = nn.CrossEntropyLoss
-CRITERIA.register_module(CrossEntropyLoss)
